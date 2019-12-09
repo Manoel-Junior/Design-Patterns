ea4bc0591de01300e962c48d0058e6395d0804af
Manoel-Junior <36748985+Manoel-Junior@users.noreply.github.com>
2019-12-09 16:42:37 -0300

    Add files via upload

---
+++ Command/src/CommandDemo.java
@@ -0,0 +1,16 @@
+
+public class CommandDemo {
+	public static void main(final String[] arguments) {
+        Light lamp = new Light();
+
+        Command switchOn = lamp::turnOn;
+        Command switchOff = lamp::turnOff;
+
+        Switch mySwitch = new Switch();
+        mySwitch.register("on", switchOn);
+        mySwitch.register("off", switchOff);
+
+        mySwitch.execute("on");
+        mySwitch.execute("off");
+    }
+}
